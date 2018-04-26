9cc1437f194fb9a39936aedd0aa11c6493b7b40e
idaliaramos <IDALIARAMOS2@GMAIL.COM>
2018-04-26 14:39:41 -0700

    solution 2 for urlify

--- chapterOne/URLify.js
+++ chapterOne/URLify.js
@@ -1,6 +1,26 @@
 //i: string
 //output: string with spaces substituted for %20
+//solution one with replace method
 function URLify(string) {
   //trim the string
-  st;
+  string = string.trim();
+  //replace
+  let url = string.replace(/\s/gi, "%20");
+
+  return url;
+}
+
+//solution 2 not using a replace method
+
+function URLify2(string) {
+  string = string.trim();
+  let newString = "";
+  for (let x = 0; x < string.length; x++) {
+    if (string.charAt(x) === " ") {
+      newString += "%20";
+    } else {
+      newString += string.charAt(x);
+    }
+  }
+  return newString;
 }
